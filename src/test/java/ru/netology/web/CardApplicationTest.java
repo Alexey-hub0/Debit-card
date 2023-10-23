cad05f6465faf6684dbe66b6f9d35441fac3b1de
Alexey Test <yashin.aleksey.job@gmail.com>
2023-10-23 14:32:17 +0300

    initial commit

---
+++ src/test/java/ru/netology/web/CardApplicationTest.java
@@ -0,0 +1,54 @@
+package ru.netology.web;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CardApplicationTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage" );
+        options.addArguments("--no-sandbox" );
+        options.addArguments("--headless" );
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void formSubmitTest() throws InterruptedException {
+        driver.get("http://localhost:9999/" );
+        WebElement form = driver.findElement(By.className("form" ));
+        form.findElement(By.cssSelector("[data-test-id=name] input" )).sendKeys("Алексей Яшин" );
+        form.findElement(By.cssSelector("[data-test-id=phone] input" )).sendKeys("+79999999999" );
+        form.findElement(By.cssSelector("[data-test-id=agreement]" )).click();
+        form.findElement((By.className("button" ))).click();
+        String text = driver.findElement((By.cssSelector("[data-test-id=order-success]" ))).getText();
+        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+        Thread.sleep(5000);
+    }
+}
+
